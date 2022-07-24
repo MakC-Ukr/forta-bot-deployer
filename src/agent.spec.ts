09a3d39f60592402a680a59d6665ec24eed319f9
maksimjeet20566 <98419737+MakC-Ukr@users.noreply.github.com>
2022-07-25 00:08:31 +0530

    type: fix
    fixing minor details that follow Nethermind project standards

--- src/agent.spec.ts
+++ src/agent.spec.ts
@@ -9,21 +9,34 @@ import {
 import agent, {
   AGENT_DEPLOYER_FUNC,
   FORTA_ADDR,
-  NETHERMIND_ADDR
+  NETHERMIND_ADDR,
 } from "./agent";
 import { TestTransactionEvent } from "forta-agent-tools/lib/tests";
 import Web3 from "web3";
 
-
 let web3 = new Web3();
-let txnData = web3.eth.abi.encodeFunctionSignature({
-  name: 'createAgent',
-  type: 'function',
-  inputs: [{type: 'uint256',name:""}, {type: 'address',name:""}, {type: 'string',name:""}, {type: 'uint256[]', name:""}]
-}) 
-+ web3.eth.abi.encodeParameters(['uint256', 'address', 'string', 'uint256[]'], 
-        ['2345675643', '0x56D9e2Ce76F9E97337938112230B1Ca3506A858f', 'Hello world', ['9696','2343']]).slice(2,);
-
+let txnData =
+  web3.eth.abi.encodeFunctionSignature({
+    name: "createAgent",
+    type: "function",
+    inputs: [
+      { type: "uint256", name: "" },
+      { type: "address", name: "" },
+      { type: "string", name: "" },
+      { type: "uint256[]", name: "" },
+    ],
+  }) +
+  web3.eth.abi
+    .encodeParameters(
+      ["uint256", "address", "string", "uint256[]"],
+      [
+        "2345675643",
+        "0x56D9e2Ce76F9E97337938112230B1Ca3506A858f",
+        "Hello world",
+        ["9696", "2343"],
+      ]
+    )
+    .slice(2);
 
 describe("Testing basic functionanlity of bot deployment detection bot", () => {
   let handleTransaction: HandleTransaction;
@@ -34,28 +47,30 @@ describe("Testing basic functionanlity of bot deployment detection bot", () => {
   });
 
   describe("Tests #1", () => {
-    
     // it("No findings if agent is not deployed", async () => {
     //   const mockTxFunc = new TestTransactionEvent().setFrom(NETHERMIND_ADDR).setTo(FORTA_ADDR);
     //   const findings = await handleTransaction(mockTxFunc);
     //   expect(findings).toStrictEqual([]);
     // });
 
-
     it("Notices agent deployment", async () => {
-      const mockTxFunc = new TestTransactionEvent().setFrom(NETHERMIND_ADDR).setTo(FORTA_ADDR).setData(txnData);
+      const mockTxFunc = new TestTransactionEvent()
+        .setFrom(NETHERMIND_ADDR)
+        .setTo(FORTA_ADDR)
+        .setData(txnData);
       console.log("HERR", FORTA_ADDR);
       const findings = await handleTransaction(mockTxFunc);
       expect(findings).toStrictEqual([
         Finding.fromObject({
-          name: ("Forta Agent Deployed"),
-          description: "A Forta agent was just deployed from the Nethermind deployer",
-          alertId: ("FAD"+'0'),
+          name: "Forta Agent Deployed",
+          description:
+            "A Forta agent was just deployed from the Nethermind deployer",
+          alertId: "FAD" + "0",
           protocol: "Forta",
           severity: FindingSeverity.Low,
           type: FindingType.Info,
-          metadata:{}
-        })
+          metadata: {},
+        }),
       ]);
     });
 
@@ -70,7 +85,5 @@ describe("Testing basic functionanlity of bot deployment detection bot", () => {
     //   const findings = await handleTransaction(mockTxFunc);
     //   expect(findings).toStrictEqual([]);
     // });
-
-
   });
 });
--- src/agent.ts
+++ src/agent.ts
@@ -8,44 +8,48 @@ import {
   FindingType,
 } from "forta-agent";
 
-let AGENT_DEPLOYER_FUNC: string = "function createAgent(uint256 agentId, address owner, string metadata, uint256[] chainIds)"
+let AGENT_DEPLOYER_FUNC: string =
+  "function createAgent(uint256 agentId, address owner, string metadata, uint256[] chainIds)";
 const FORTA_ADDR: string = "0x61447385B019187daa48e91c55c02AF1F1f3F863";
 const NETHERMIND_ADDR: string = "0x88dc3a2284fa62e0027d6d6b1fcfdd2141a143b8";
 
 let findingsCount = 0;
 
+const handleTransaction: HandleTransaction = async (
+  txEvent: TransactionEvent
+) => {
+  const findings: Finding[] = [];
 
-const handleTransaction: HandleTransaction = async(txEvent: TransactionEvent) => {
-    const findings: Finding[] = [];
-
-    if(txEvent.from?.toLowerCase() != NETHERMIND_ADDR?.toLowerCase()){
-      return findings;
-    }
-
-    if(txEvent.to?.toLowerCase() != FORTA_ADDR?.toLowerCase()){
-      return findings;
-    }
-
-    const txns = txEvent.filterFunction(AGENT_DEPLOYER_FUNC, FORTA_ADDR);
-    txns.forEach((_) => { 
-        findings.push(Finding.fromObject({
-          name: ("Forta Agent Deployed"),
-          description: "A Forta agent was just deployed from the Nethermind deployer",
-          alertId: ("FAD"+findingsCount.toString()),
-          protocol: "Forta",
-          severity: FindingSeverity.Low,
-          type: FindingType.Info,
-          metadata:{}
-        }))
-        findingsCount+=1;
-    })
-
+  if (txEvent.from?.toLowerCase() != NETHERMIND_ADDR?.toLowerCase()) {
     return findings;
   }
+
+  if (txEvent.to?.toLowerCase() != FORTA_ADDR?.toLowerCase()) {
+    return findings;
+  }
+
+  const txns = txEvent.filterFunction(AGENT_DEPLOYER_FUNC, FORTA_ADDR);
+  txns.forEach((_) => {
+    findings.push(
+      Finding.fromObject({
+        name: "Forta Agent Deployed",
+        description:
+          "A Forta agent was just deployed from the Nethermind deployer",
+        alertId: "FAD" + findingsCount.toString(),
+        protocol: "Forta",
+        severity: FindingSeverity.Low,
+        type: FindingType.Info,
+        metadata: {},
+      })
+    );
+    findingsCount += 1;
+  });
+
+  return findings;
+};
 
 export default {
   handleTransaction,
 };
-  
 
-export {AGENT_DEPLOYER_FUNC, FORTA_ADDR, NETHERMIND_ADDR};
+export { AGENT_DEPLOYER_FUNC, FORTA_ADDR, NETHERMIND_ADDR };
